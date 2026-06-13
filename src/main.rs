a5662efad7a92a0a902e1e1e56e87b72e25e08bd
Paul8711 <154304377+Paulprojects8711@users.noreply.github.com>
2026-06-13 17:56:37 +0200

    feat: add window icon

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,42 @@
+use macroquad::miniquad::conf::Icon;
 use macroquad::prelude::*;
 
-#[macroquad::main("Shadow Dash")]
+fn window_conf() -> Conf {
+    let icon = Icon {
+        small: *include_bytes!("../assets/sprites/icon/icon_16.rgba"),
+        medium: *include_bytes!("../assets/sprites/icon/icon_32.rgba"),
+        big: *include_bytes!("../assets/sprites/icon/icon_64.rgba"),
+    };
+    Conf {
+        window_title: String::from("Shadow Dash Demo"),
+        fullscreen: true,
+        window_resizable: false,
+        icon: Some(icon),
+        ..Default::default()
+    }
+}
+
+// TODO:
+// - include_bytes for all assets
+// - use cargo-bundle for... well bundling
+
+#[macroquad::main(window_conf)]
 async fn main() {
-    set_fullscreen(true);
     let font = load_ttf_font("assets/fonts/lubbartz.ttf").await.unwrap();
-    let bg = load_texture("assets/sprites/bg1/bg1.png").await.unwrap();
     let player = load_texture("assets/sprites/spr_player/spr_player.png")
         .await
         .unwrap();
     loop {
+        // delta time: makes for example speed dependent on seconds NOT on frames:
+        // x += speed * delta_time
+        let delta_time = get_frame_time();
+
+        // clear_background instead of texture so it covers the entire screen
+        clear_background(Color::from_hex(0x00AEF0));
+
         // since the texture covers the entire screen, all texture x and y values are in the top
         // left corner (this will be fun later)
-        draw_texture(&bg, 0.0, 0.0, WHITE);
+        // draw_texture(&bg, 0.0, 0.0, WHITE);
 
         let textparams = TextParams {
             font: Some(&font),
