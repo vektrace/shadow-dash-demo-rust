9088f9e5ce471224f4c9fbfc25ac296bf41fbba8
Redon4 <211563626+redon4@users.noreply.github.com>
2026-06-21 12:10:53 +0200

    feat: add basic movment

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use macroquad::input::KeyCode;
 use macroquad::miniquad::conf::Icon;
 use macroquad::prelude::*;
 
@@ -22,17 +23,23 @@ fn window_conf() -> Conf {
 
 #[macroquad::main(window_conf)]
 async fn main() {
+    const SPEED: f32 = 250.0;
+
+    let mut x = 250.0;
+    let mut y = 250.0;
+
     let font = load_ttf_font("assets/fonts/lubbartz.ttf").await.unwrap();
     let player = load_texture("assets/sprites/spr_player/spr_player.png")
         .await
         .unwrap();
+
     loop {
         // delta time: makes for example speed dependent on seconds NOT on frames:
         // x += speed * delta_time
         let delta_time = get_frame_time();
 
         // clear_background instead of texture so it covers the entire screen
-        clear_background(Color::from_hex(0x00AEF0));
+        clear_background(Color::from_hex(0x0000_AEF0));
 
         // since the texture covers the entire screen, all texture x and y values are in the top
         // left corner (this will be fun later)
@@ -43,9 +50,19 @@ async fn main() {
             font_size: 64,
             ..Default::default()
         };
-        draw_texture(&player, 250.0, 250.0, WHITE);
+        draw_texture(&player, x, y, WHITE);
         draw_text_ex("HELLO", 200.0, 200.0, textparams);
 
-        next_frame().await
+        // TODO:
+        // - add keybinds with 2 options at the same time
+        // - add helper fn for shorter code when doing 2 keys
+
+        if is_key_down(KeyCode::Left) || is_key_down(KeyCode::A) {
+            x -= SPEED * delta_time;
+        } else if is_key_down(KeyCode::Right) || is_key_down(KeyCode::D) {
+            x += SPEED * delta_time;
+        }
+
+        next_frame().await;
     }
 }
