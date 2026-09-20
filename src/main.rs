94c8fc3170220f43cd79ab6111eccf3bd3074648
redon4 <211563626+redon4@users.noreply.github.com>
2026-09-20 15:23:51 +0200

    feat: add fps debug and label can_jump debug

--- src/main.rs
+++ src/main.rs
@@ -119,10 +119,16 @@ impl Game {
         );
         draw_all(&self.objects);
         draw_text_ex(
-            format!("{}", self.player.can_jump),
+            format!("can_jump: {}", self.player.can_jump),
+            10.,
             200.0,
-            200.0,
-            textparams,
+            textparams.clone(),
+        );
+        draw_text_ex(
+            format!("fps: {}", get_fps()),
+            10.,
+            30.,
+            textparams.clone(),
         );
     }
 }
